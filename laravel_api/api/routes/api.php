099d8fba78d307c632e4dd3b0c08bfdab669ef33
Samuel Moncada Salazar <samoncadas@unal.edu.co>
2026-02-27 11:44:35 -0500

    creacion de modelo, migracion y controlador  de character

--- laravel_api/api/routes/api.php
+++ laravel_api/api/routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CharacterController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +15,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::get('character', [CharacterController::class, 'index']);
+Route::post('character', [CharacterController::class, 'store']);
+Route::get('character/{id}', [CharacterController::class, 'show']);
